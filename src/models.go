557ba45ee9a7b0fecdc57f6d404d63cc89cac405
jerysun <jerysun007@hotmail.com>
2025-04-14 21:56:49 +0200

    add posts table and endpoint

--- src/models.go
+++ src/models.go
@@ -76,16 +76,55 @@ func databaseFeedFollowToFeedFollow(feedFollow database.FeedFollow) FeedFollow {
 }
 
 func databaseFeedFollowsToFeedFollows(feedFollows []database.FeedFollow) []FeedFollow {
-  goFeedFollows := make([]FeedFollow, len(feedFollows))
+  result := make([]FeedFollow, len(feedFollows))
   for i, feedFollow := range feedFollows {
-    goFeedFollows[i] = databaseFeedFollowToFeedFollow(feedFollow)
+    result[i] = databaseFeedFollowToFeedFollow(feedFollow)
   }
-  return goFeedFollows
+  return result
+}
+
+type Post struct {
+  ID          uuid.UUID  `json:"id"`
+  CreatedAt   time.Time  `json:"created_at"`
+  UpdatedAt   time.Time  `json:"updated_at"`
+  Title       string     `json:"title"`
+  Url         string     `json:"url"`
+  Description *string    `json:"description"`
+  PublishedAt *time.Time `json:"published_at"`
+  FeedID      uuid.UUID  `json:"feed_id"`
+}
+
+func databasePostToPost(post database.Post) Post {
+  return Post{
+    ID:          post.ID,
+    CreatedAt:   post.CreatedAt,
+    UpdatedAt:   post.UpdatedAt,
+    Title:       post.Title,
+    Url:         post.Url,
+    Description: nullStringToStringPtr(post.Description),
+    PublishedAt: nullTimeToTimePtr(post.PublishedAt),
+    FeedID:      post.FeedID,
+  }
+}
+
+func databasePostsToPosts(posts []database.Post) []Post {
+  result := make([]Post, len(posts))
+  for i, post := range posts {
+    result[i] = databasePostToPost(post)
+  }
+  return result
 }
 
 func nullTimeToTimePtr(t sql.NullTime) *time.Time {
   if t.Valid {
     return &t.Time
+  }
+  return nil
+}
+
+func nullStringToStringPtr(s sql.NullString) *string {
+  if s.Valid {
+    return &s.String
   }
   return nil
 }
